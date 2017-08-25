be04353aa78ea1b2702a2f58486abe8a1f5863ed
Sonyy172 <phuc.nh94@gmail.com>
2017-08-25 21:34:25 +0700

    a

--- CoreChatbot/TheVoiceKid/vote_guess_menu.py
+++ CoreChatbot/TheVoiceKid/vote_guess_menu.py
@@ -14,27 +14,26 @@ db = client.voted_users
 
 def vote_guess_menu(sender_id):
     voters = db.voters
-    check_voter = voters.find_one({'id_user': 321321})
+    check_voter = voters.find_one({'id_user': sender_id})
     if bool(check_voter):
         page.send(sender_id, "User da binh chon")
+        page.send(sender_id, check_voter["HLV_da_binh_chon"])
+
     else:
         page.send(sender_id, "User chua binh chon")
+        page.send(sender_id, Attachment.Image(
+            "http://210.211.109.211/weqbfyretnccbsaf/home_hinh3_du_doan.jpg"))
 
-    # page.send(sender_id, check_voter["first_name"])
-
-    page.send(sender_id, Attachment.Image(
-        "http://210.211.109.211/weqbfyretnccbsaf/home_hinh3_du_doan.jpg"))
-
-    # gui cac quick reply
-    question = "Bạn dự đoán thí sinh thuộc đội của huấn luyện viên nào sẽ xuất sắc giành lấy ngôi vị quán quân của chương trình?"
-    quick_replies = [
-        QuickReply(title="#teamcôTường", payload="Vũ Cát Tường"),
-        QuickReply(title="#teamcôTiênvàcôTràm", payload="Tiên Cookie và Hương Tràm"),
-        QuickReply(title="#teamchúSoobin", payload="Soobin")
-    ]
-    page.send(sender_id,
-              question,
-              quick_replies=quick_replies,
-              metadata="DEVELOPER_DEFINED_METADATA")
+        # gui cac quick reply
+        question = "Bạn dự đoán thí sinh thuộc đội của huấn luyện viên nào sẽ xuất sắc giành lấy ngôi vị quán quân của chương trình?"
+        quick_replies = [
+            QuickReply(title="#teamcôTường", payload="Vũ Cát Tường"),
+            QuickReply(title="#teamcôTiênvàcôTràm", payload="Tiên Cookie và Hương Tràm"),
+            QuickReply(title="#teamchúSoobin", payload="Soobin")
+        ]
+        page.send(sender_id,
+                  question,
+                  quick_replies=quick_replies,
+                  metadata="DEVELOPER_DEFINED_METADATA")
 
     return
