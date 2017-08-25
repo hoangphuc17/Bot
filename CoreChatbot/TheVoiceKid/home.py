a5ca2cbbae4423cdde34ee5a9fba49db747f2576
Sonyy172 <phuc.nh94@gmail.com>
2017-08-25 10:26:00 +0700

    a

--- CoreChatbot/TheVoiceKid/home.py
+++ CoreChatbot/TheVoiceKid/home.py
@@ -22,39 +22,34 @@ def home(sender_id):
                                 ]),
         Template.GenericElement("Video Full - The Voice Kids 2017 | Giọng Hát Việt Nhí mùa 5",
                                 subtitle="Xem lại bản đầy dủ các tập đã được phát sóng trên Youtube, Live Streaming",
-                                image_url="http://2sao.vietnamnetjsc.vn/images/2017/06/14/08/58/the-face-2017.png",
+                                image_url="http://210.211.109.211/weqbfyretnccbsaf/hinh2_home_xem_video.jpg",
                                 buttons=[
                                     Template.ButtonWeb(
-                                        "Xem trên Youtube", "https://www.youtube.com/playlist?list=PLEhBV4sOYnBmMRpojxlAB4pcOA9V18B8J"),
+                                        "Xem trên Youtube", "https://www.youtube.com/user/btcgionghatvietnhi"),
                                     Template.ButtonWeb(
-                                        "Xem trên Youtube", "https://www.youtube.com/playlist?list=PLEhBV4sOYnBmMRpojxlAB4pcOA9V18B8J")
+                                        "Xem trên Facebook", "https://www.youtube.com/user/btcgionghatvietnhi")
                                 ]),
-        Template.GenericElement("Bình chọn thí sinh yêu thích nhất hàng tuần",
-                                subtitle="Hãy bình chọn cho thí sinh bạn yêu thích nhất hàng tuần bạn nhé!",
+        Template.GenericElement("Dự đoán kết quả và giành lấy cơ hội nhận quà",
+                                subtitle="Tham gia dự đoán kết quả của cuộc thi để nhận được những phần quà hấp dẫn nhất từ ban tổ chức",
                                 image_url="https://img.saostar.vn/2017/03/28/1168861/thefaceonline-concept-milor-hoangku.jpg",
                                 buttons=[
                                     Template.ButtonPostBack(
-                                        "Bình chọn", "binh_chon"),
+                                        "Tham gia dự đoán 👍", "vote_guess"),
                                     Template.ButtonPostBack(
-                                        "Thể lệ bình chọn", "the_le_binh_chon")
-                                ]),
-        Template.GenericElement("Video Full - The Face Vietnam 2017",
-                                subtitle="Xem lại bản đầy đủ các tập đã được phát sóng trên Youtube, Live Streaming.",
-                                image_url="https://i.ytimg.com/vi/i9nFRcKPA4I/maxresdefault.jpg",
-                                buttons=[
-                                    Template.ButtonWeb(
-                                        "Xem trên Youtube", "https://www.youtube.com/playlist?list=PLEhBV4sOYnBmMRpojxlAB4pcOA9V18B8J")
+                                        "Thể lệ dự đoán 📜", "vote_rule")
                                 ]),
         Template.GenericElement("About us",
-                                subtitle="Theo dõi The Face Vietnam 2017 thông qua các kênh truyền thông",
+                                subtitle="Theo dõi chương trình Giọng Hát Việt Nhí 2017 tại các kênh truyền thông",
                                 image_url="http://static.vietnammoi.vn/stores/news_dataimages/duynt/042017/17/16/1002_the-face-vietnam.jpg",
                                 buttons=[
                                     Template.ButtonWeb(
-                                        "Youtube", "https://www.youtube.com/channel/UCF5RuEuoGrqGtscvLGLOMew/featured"),
+                                        "Youtube", "https://www.youtube.com/user/btcgionghatvietnhi"),
                                     Template.ButtonWeb(
-                                        "Instagram", "https://www.instagram.com/thefacevietnam_official/"),
-                                    Template.ButtonWeb(
-                                        "Facebook", "https://www.facebook.com/TheFaceVietNamOfficial/")
+                                        "Facebook", "https://www.facebook.com/gionghatvietnhi/"),
+                                    Template.ButtonPostBack(
+                                        "Giờ phát sóng", "about_us_timeline"),
+                                    Template.ButtonPostBack(
+                                        "Tổng quan về chương trình", "about_us_introduce")
 
                                 ])
     ]
