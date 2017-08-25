5739176be93e559a65bf1a2d6580b74cde88430a
Sonyy172 <phuc.nh94@gmail.com>
2017-08-26 02:07:51 +0700

    a

--- CoreChatbot/TheVoiceKid/vote.py
+++ CoreChatbot/TheVoiceKid/vote.py
@@ -84,7 +84,7 @@ def vote_handle_quick_reply(sender_id, quick_reply_payload):
 
     if bool(check_voter):
         # update
-        voters.update_one({'_id': check_voter._id}, {"$set": voter}, upsert=False)
+        voters.update_one({'_id': check_voter}, {"$set": voter}, upsert=False)
         page.send(sender_id, HLV_da_binh_chon)
 
     else:
