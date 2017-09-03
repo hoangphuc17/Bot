c0c252ea044b8eccca163aa96c0f6c413bcde37a
Sonyy172 <phuc.nh94@gmail.com>
2017-09-03 14:39:03 +0700

    a

--- ApiMessenger/__init__.py
+++ ApiMessenger/__init__.py
@@ -1,5 +1,5 @@
-# __version__ = '2.1.0'
-#
-# from ApiMessenger import *
-# from . import attachment as Attachment
-# from . import template as Template
+__version__ = '2.1.0'
+
+from ApiMessenger import *
+from . import attachment as Attachment
+from . import template as Template
