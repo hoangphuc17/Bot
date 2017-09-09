f51e3707f4fa3b4c40300d5949e9e631ceced751
Sonyy172 <phuc.nh94@gmail.com>
2017-09-09 07:24:45 +0700

    a

--- CoreChatbot/TheVoiceKid/database.py
+++ CoreChatbot/TheVoiceKid/database.py
@@ -11,7 +11,7 @@ from CoreChatbot.Preparation.fbpage import page
 
 import datetime
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017, connect=True)
+client = MongoClient('localhost', 27017)
 db = client.Phuc
 
 USER = db.USER
