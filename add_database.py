2bd612dbdc3c10e3c930083a4c2f1e88af42f3ee
Sonyy172 <phuc.nh94@gmail.com>
2017-09-13 21:22:58 +0700

    a

--- add_database.py
+++ add_database.py
@@ -36,6 +36,7 @@ def insert_new_questions():
                     "HLV Giọng Hát Việt Nhí 2017 bao gồm ghế đôi ca sĩ Hương Tràm & nhạc sĩ Tiên Cookie, ghế đơn ca sĩ Soobin Hoàng Sơn và sự trở lại của HLV Giọng Hát Việt Nhí 2016 Vũ Cát Tường. Theo dõi chương trình và ủng hộ các đội mà bạn yêu thích nhé! ❤", "")
     insert_question(["tuyển"], "Làm sao để đăng ký tham gia Giọng Hát Việt Nhí ?",
                     "Để đăng ký tham gia chương trình bạn vui lòng truy cập vào website chính thức Giọng Hát Việt Nhí / The Voice Kids Viet Nam: http://gionghatvietnhi.com.vn/ và theo dõi Fanpage chính thức Giọng Hát Việt Nhí để cập nhật thông tin mới nhất bạn nhé!", "")
+    print('da them new question')
 
 
 def insert_new_news():
@@ -43,6 +44,7 @@ def insert_new_news():
                 "https://img.saostar.vn/265x149/2017/08/19/1500005/8.jpg", "https://saostar.vn/tv-show/sau-thuy-binh-vu-cat-tuong-lai-chieu-mo-thanh-cong-hoang-tu-dan-ca-tam-hao-1500005.html")
     insert_news("Thể hiện hit của diva Hà Trần, ‘thiên thần nhí’ khiến Soobin, Vũ Cát Tường phải tung ‘chiêu’ hết mình chinh phục", "Lần đầu tiên ở mùa giải năm nay, Giọng hát Việt nhí 2017 đã có một thí sinh khiến các HLV phải tung hết tất cả các chiêu trò để chiêu dụ về đội của mình. ",
                 "https://img.saostar.vn/265x149/2017/08/19/1500621/mg_8085.jpg", "https://saostar.vn/tv-show/hien-hit-cua-diva-ha-tran-thien-nhi-khien-soobin-vu-cat-tuong-phai-tung-chieu-het-minh-chinh-phuc-1500621.html")
+    print('da them new news')
 
 
 insert_new_news()
