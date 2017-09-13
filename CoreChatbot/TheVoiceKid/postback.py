c9a374a777cc6756da4450072cafa34911c41bc8
Sonyy172 <phuc.nh94@gmail.com>
2017-09-13 15:35:47 +0700

    a

--- CoreChatbot/TheVoiceKid/postback.py
+++ CoreChatbot/TheVoiceKid/postback.py
@@ -364,6 +364,30 @@ def minigame2_handle_result(message, sender_id):
             Template.ButtonPostBack("Home", "home")
         ]
         page.send(sender_id, Template.Buttons(text, buttons))
-    # elif message in vu_cat_tuong:
-    # elif message in soobin:
-    # elif message in tien_cookie:
+    elif message in vu_cat_tuong:
+        game2_hlv_url = "http://210.211.109.211/weqbfyretnccbsaf/game2_vucattuong.jpg"
+        page.send(sender_id, Attachment.Image(game2_hlv_url))
+        text = "Chính xác!!!!!!!!"
+        buttons = [
+            Template.ButtonPostBack("Đoán thêm lần nữa 😻", "minigame2_menu"),
+            Template.ButtonPostBack("Home", "home")
+        ]
+        page.send(sender_id, Template.Buttons(text, buttons))
+    elif message in soobin:
+        game2_hlv_url = "http://210.211.109.211/weqbfyretnccbsaf/game2_soobin.jpg"
+        page.send(sender_id, Attachment.Image(game2_hlv_url))
+        text = "Chính xác!!!!!!!!"
+        buttons = [
+            Template.ButtonPostBack("Đoán thêm lần nữa 😻", "minigame2_menu"),
+            Template.ButtonPostBack("Home", "home")
+        ]
+        page.send(sender_id, Template.Buttons(text, buttons))
+    elif message in tien_cookie:
+        game2_hlv_url = "http://210.211.109.211/weqbfyretnccbsaf/game2_tiencookie.jpg"
+        page.send(sender_id, Attachment.Image(game2_hlv_url))
+        text = "Chính xác!!!!!!!!"
+        buttons = [
+            Template.ButtonPostBack("Đoán thêm lần nữa 😻", "minigame2_menu"),
+            Template.ButtonPostBack("Home", "home")
+        ]
+        page.send(sender_id, Template.Buttons(text, buttons))
