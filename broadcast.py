da3bb746cafa9f61671732a6e45ead3aee98375e
Sonyy172 <phuc.nh94@gmail.com>
2017-09-19 09:29:57 +0700

    a

--- broadcast.py
+++ broadcast.py
@@ -51,7 +51,11 @@ def send_video_broadcast(sender_id):
     page.send(sender_id, Attachment.Video(url_video))
 
 
-send_video_broadcast(id_phuc)
+check = USER.find_one({'id_user': id_phuc})
+    if bool(check):
+        send_video_broadcast(id_phuc)
+    else:
+        pass
 
 # for user in USER.find():
 #     send_video_broadcast(id_phuc)
