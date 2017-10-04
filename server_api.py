00978e7aa87b74ecd7a91011dc911543fab2303c
Sonyy172 <phuc.nh94@gmail.com>
2017-10-04 14:44:16 +0700

    a

--- server_api.py
+++ server_api.py
@@ -249,13 +249,15 @@ def upload_file():
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
-            return redirect(request.url)
+            # return redirect(request.url)
+            return 'file not in format'
         file = request.files['file']
         # if user does not select file, browser also
         # submit a empty part without filename
         if file.filename == '':
             flash('No selected file')
-            return redirect(request.url)
+            # return redirect(request.url)
+            return 'No selected file'
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
