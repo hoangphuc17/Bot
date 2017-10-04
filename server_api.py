d005ce3efa18482f9cef4f3f5541575eec936a69
Sonyy172 <phuc.nh94@gmail.com>
2017-10-04 15:01:12 +0700

    a

--- server_api.py
+++ server_api.py
@@ -244,25 +244,30 @@ def allowed_file(filename):
 
 
 @app.route('/broadcast/upload', methods=['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         # check if the post request has the file part
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             # return redirect(request.url)
+#             return 'file not in format'
+#         file = request.files['file']
+#         # if user does not select file, browser also
+#         # submit a empty part without filename
+#         if file.filename == '':
+#             flash('No selected file')
+#             # return redirect(request.url)
+#             return 'No selected file'
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             # return redirect(url_for('uploaded_file', filename=filename))
+#             return ('http://210.211.109.211/weqbfyretnccbsaf/' + filename)
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files['file']:
-            flash('No file part')
-            # return redirect(request.url)
-            return 'file not in format'
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            # return redirect(request.url)
-            return 'No selected file'
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('uploaded_file', filename=filename))
-            return ('http://210.211.109.211/weqbfyretnccbsaf/' + filename)
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
 
 
 # @app.route('/broadcast/upload', methods=['GET', 'POST'])
