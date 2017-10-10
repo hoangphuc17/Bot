34504eabc1656b04d9bfe4d8d42017f3a5530c6b
Sonyy172 <phuc.nh94@gmail.com>
2017-10-10 19:47:14 +0700

    a

--- broadcast.py
+++ broadcast.py
@@ -27,9 +27,10 @@ id_duc = "1627683190629761"
 
 
 def send_broadcast(sender_id):
-    url_broadcast_image = "http://210.211.109.211/weqbfyretnccbsaf/poster-tap6-2.jpg"
+    url_broadcast_image = "http://210.211.109.211/weqbfyretnccbsaf/broadcast1010.jpg"
     page.send(sender_id, Attachment.Image(url_broadcast_image))
-    text = "Hôm nay trời rộng lên cao. Hóng The Voice Kids nôn nao cả ngày !!! Nhớ đón xem Giọng Hát Việt Nhí vào 21h tối nay trên VTV3 nhé bạn đáng yêu ơi ! 💕💕 "
+    # text = "Hôm nay trời rộng lên cao. Hóng The Voice Kids nôn nao cả ngày !!! Nhớ đón xem Giọng Hát Việt Nhí vào 21h tối nay trên VTV3 nhé bạn đáng yêu ơi ! 💕💕 "
+    text = "CHIẾC VÉ MAY MẮN - BÌNH CHỌN THÍCH SINH BẠN YÊU THÍCH\nTop 18 Giọng hát Việt Nhí 2017 đã lộ diện, Ban Tổ Chức chính thức mở cổng bình chọn 'Chiếc Vé May Mắn'.\nCơ hội "Đặc Biệt" dành cho các giọng ca nhí bị loại trong vòng Liveshow chương trình có thể trở lại đêm Chung kết nếu nhận được Tổng lượt bình chọn cao nhất từ khán giả thông qua Zalo Page Giọng Hát Việt Nhí và Tạp chí điện tử Saostar.\n🔰 Bình chọn qua Tạp chí điện tử Saostar: http://saostar.vn\n🔰 Bình chọn qua Zalo Giọng Hát Việt Nhí: zalo.me/gionghatvietnhi\n👉 Truy cập Zalo Page Giọng Hát Việt Nhí ➡️ Nhấn 'Quan Tâm'.\n👉 Vào mục tin nhắn ➡️ Bình Chọn ➡️ Click vào ảnh thí sinh bạn yêu thích để bình chọn ➡️ Xác nhận.\n👉 Mỗi tài khoản Zalo có tối đa 18 lượt bình chọn. Lưu ý mỗi thí sinh chỉ nhận được tối đa 1 lượt bình chọn.\n🔰 Bình chọn được mở từ ngày 07/10/2017 đến ngày 18/11/2017.\n#teamVuCatTuong #teamHuongTramTienCookie #teamSoobinHoangSon\nTheo dõi Fanpage và đồng hành cùng Giọng Hát Việt Nhí 2017 tìm ra Quán quân xứng đáng các bạn nhé! ;)"
     buttons = [
         Template.ButtonPostBack("Home", "home")
     ]
@@ -57,5 +58,5 @@ def send_video_broadcast(sender_id):
 
 
 for user in USER.find():
-    send_video_broadcast(user['id_user'])
-    # send_broadcast(user['id_user'])
+    # send_video_broadcast(user['id_user'])
+    send_broadcast(user['id_user'])
