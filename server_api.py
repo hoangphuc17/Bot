722393656c694d9a5e5bb5185a26c0732b8a213b
Sonyy172 <phuc.nh94@gmail.com>
2017-11-20 16:16:05 +0700

    a

--- server_api.py
+++ server_api.py
@@ -432,7 +432,7 @@ def broadcast_save_message():
         message = request.form['message']
         dt = request.form['timestamp']
         # 'Jun 1 2005  1:33PM'
-        datetime_object = datetime.strptime(dt, '%Y-%b-%d %H:%M')
+        datetime_object = datetime.strptime(dt, '%Y-%m-%d %H:%M')
 
         # luu broadcast
         new_bc = {
