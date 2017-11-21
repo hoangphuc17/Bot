b552e6fc464d16542252cf8ad647dd7f0675834e
Sonyy172 <phuc.nh94@gmail.com>
2017-11-21 10:42:12 +0700

    a

--- server_api.py
+++ server_api.py
@@ -428,7 +428,8 @@ def get_broadcsast_by_time(time):
         # datetime_object = datetime.strptime(dt, '%Y-%m-%d %H:%M')
         # date_db = str(bc['timestamp'].date())
 
-        return bc['timestamp']
+        a = bc['timestamp']
+        return a
 
         # abc = bc['timestamp']
         # if abc == time:
