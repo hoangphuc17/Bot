0f2b998aba73a652a8e8db06ea9a84c983c57657
Sonyy172 <phuc.nh94@gmail.com>
2017-11-24 14:46:52 +0700

    a

--- CoreChatbot/cbtest/cbtest_database.py
+++ CoreChatbot/cbtest/cbtest_database.py
@@ -18,98 +18,8 @@ import datetime
 from pymongo import MongoClient
 client = MongoClient('cb.saostar.vn', 27017)
 db = client.Phuc
-FAQ3 = db.FAQ3
 FAQ4 = db.FAQ4
 SESSION = db.SESSION
-
-
-# def faq3_level_1(chatbot, id_node_level_1, keyword):
-#     node_level_1 = {
-#         'chatbot': chatbot,
-#         'id_node_level_1': id_node_level_1,
-#         'level': '1',
-#         'keyword': keyword
-#     }
-#     FAQ3.insert_one(node_level_1)
-
-
-# def faq3_level_2(chatbot, id_node_level_1, id_node_level_2, keyword):
-#     node_level_2 = {
-#         'chatbot': chatbot,
-#         'level': '2',
-#         'node_level_1': id_node_level_1,
-#         'id_node_level_2': id_node_level_2,
-#         'keyword': keyword
-#     }
-#     FAQ3.insert_one(node_level_2)
-
-
-# def faq3_answer(chatbot, id_node_level_2, answer):
-#     node_answer = {
-#         'chatbot': chatbot,
-#         'level': 'answer',
-#         'node_level_2': id_node_level_2,
-#         'answer': answer
-#     }
-#     FAQ3.insert_one(node_answer)
-
-
-# def faq3_list(lv1, lv2, ans):
-#     new_lv1 = []
-#     new_lv2 = []
-
-#     for item in lv1:
-#         new_item = word_sent(item)
-#         for it in new_item:
-#             new_lv1.append(it)
-
-#     for item in lv2:
-#         new_item = word_sent(item)
-#         for it in new_item:
-#             new_lv2.append(it)
-
-#         # new_lv1.append(item)
-
-#     lst = {
-#         'list_array': [new_lv1, new_lv2, ans]
-#     }
-#     FAQ3.insert_one(lst)
-
-
-def lv_1(id_node, id_node_parent, keyword, answer):
-    new_lv1 = {
-        'level': '1',
-        'id_node': id_node,
-        'id_node_parent': id_node_parent,
-        'keyword': keyword,
-        'answer': answer
-    }
-    FAQ4.insert_one(new_lv1)
-
-
-def lv_2(id_node, id_node_parent, keyword, answer):
-    new_lv2 = {
-        'level': '2',
-        'id_node': id_node,
-        'id_node_parent': id_node_parent,
-        'keyword': keyword,
-        'answer': answer
-    }
-    FAQ4.insert_one(new_lv2)
-
-
-def lv_3(id_node, id_node_parent, keyword, answer):
-    new_lv3 = {
-        'level': '3',
-        'id_node': id_node,
-        'id_node_parent': id_node_parent,
-        'keyword': keyword,
-        'answer': answer
-    }
-    FAQ4.insert_one(new_lv3)
-
-
-# def check_session():
 
 
 def node(level, id_node, list_id_node_parents, keyword, answer):
@@ -123,14 +33,24 @@ def node(level, id_node, list_id_node_parents, keyword, answer):
     FAQ4.insert_one(new_node)
 
 
-def list_of_keyword():
-    list_keyword = []
-    for each_node in FAQ4.find():
-        for keyword in each_node['keyword']:
-            list_keyword.append({
-                'keyword': keyword,
-                'id_node': each_node['id_node'],
-                'id_node_parents': each_node['id_node_parents']
-            })
+def add_node():
+    node('1', '1', [], 'mua', '')
+    node('2', '11', ['1'], 'áo', '')
+    node('3', '111', ['1', '11'], 'm', 'A')
+    node('3', '112', ['1', '11'], 'l', 'B')
 
-    return list_keyword
+    node('2', '12', ['1'], 'giày', '')
+    node('3', '121', ['1', '12'], '8', 'C')
+    node('3', '122', ['1', '12'], '9', 'D')
+
+    node('1', '2', [], 'bán', '')
+    node('2', '21', ['2', '21'], 'áo', '')
+    node('3', '211', ['2', '21'], 'm', 'E')
+    node('3', '212', ['2', '21'], 'l', 'F')
+
+    node('2', '22', ['2'], 'giày', '')
+    node('3', '221', ['2', '22'], '8', 'G')
+    node('3', '222', ['2', '22'], '9', 'H')
+
+
+add_node()
--- add_db_cbtest.py
+++ add_db_cbtest.py
@@ -3,39 +3,5 @@ import sys
 from CoreChatbot.cbtest.cbtest_database import *
 
 
-# def add_faq3():
-#     faq3_level_1('test', '1', ['tổ chức', 'diễn ra'])
-#     faq3_level_2('test', '1', '11', ['địa điểm', 'ở đâu', 'tại', 'chỗ', 'nơi'])
-#     faq3_answer('test', '11', '179 LCT')
-
-#     faq3_level_1('test', '1', ['tổ chức', 'diễn ra'])
-#     faq3_level_2('test', '1', '12', ['khi', 'lúc', 'bao', 'giờ', 'thời điểm'])
-#     faq3_answer('test', '12', 'Tháng 10 hàng năm')
-
-#     faq3_level_1('test', '2', ['địa điểm', 'nơi', 'chỗ'])
-#     faq3_level_2('test', '2', '21', ['quay', 'ghi'])
-#     faq3_answer('test', '21', '5B NDC')
-
-#     faq3_level_1('test', '2', ['địa điểm', 'nơi', 'chỗ'])
-#     faq3_level_2('test', '2', '22', [
-#                  'tham gia', 'đăng kí', 'đăng ký', 'tham gia'])
-#     faq3_answer('test', '21', 'chỉ ở TPHCM')
-
-
-# add_faq3()
-
-# def add_faq3_list():
-#     faq3_list(['tổ chức', 'diễn ra'], ['địa điểm',
-#                                        'ở đâu', 'tại', 'chỗ', 'nơi'], '179 LCT')
-#     faq3_list(['tổ chức', 'diễn ra'], ['khi', 'lúc', 'bao',
-#                                        'giờ', 'thời điểm'], 'Tháng 10 hàng năm')
-#     faq3_list(['địa điểm', 'nơi', 'chỗ', 'ở đâu'],
-#               ['quay', 'ghi hình'], '5B NDC')
-#     faq3_list(['địa điểm', 'nơi', 'chỗ', 'ở đâu'], ['tham gia', 'tuyển sinh',
-#                                                     'đăng kí', 'đăng ký', 'tham gia'], 'chỉ ở TPHCM')
-
-
-# add_faq3_list()
-
-
-# def add_db():
+def add_db():
+    node('1',)
